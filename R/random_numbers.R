fba8cba62b396421b570a7996977c8464be3cb80
Panagiotis Stylianos <pete.styl@gmail.com>
2020-10-19 07:07:12 +0300

    package website

---
+++ R/random_numbers.R
@@ -0,0 +1,9 @@
+#' Random Numbers Generator
+#'
+#' This function produces 10 random numbers.
+#'
+#' More specifically the numbers come from the standard normal distribution.
+#'
+#' @usage Random Numbers for random tests
+#' @source Coronavirus package \url{https://panagiotis-stylianos.netlify.app}{Web}
+"random_numbers"
